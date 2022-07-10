c4da36a28ce507c950d3e5574baace8f4ed117e5
Ayesha Bhatti <abhatt13@mail.bbk.ac.uk>
2022-07-10 14:20:57 +0100

    Attempting to link backend to frontend

--- my-app/src/components/Nav.js
+++ my-app/src/components/Nav.js
@@ -9,41 +9,18 @@ function Nav() {
   };
 
   return (
-    <div>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a style={navStyle} class="navbar-brand" href="#">Brand Logo</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+    <nav>
+        <h3>Logo</h3>  
+        <ul className="nav-links">
+          <Link style={navStyle} to='/'>
+            <li>Home</li>
+          </Link>
+          <Link style={navStyle} to='/matter'>
+            <li>Matter</li>
+          </Link>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a style={navStyle} class="nav-link" href="#">Home<span class="sr-only"></span></a>
-      </li>
-      <li class="nav-item dropdown">
-        <a style={navStyle} class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Case List
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a style={navStyle} class="dropdown-item" href="#">Property</a>
-          <a style={navStyle} class="dropdown-item" href="#">Litigation</a>
-          <div class="dropdown-divider"></div>
-          <a style={navStyle} class="dropdown-item" href="#">Torts</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a style={navStyle} class="nav-link disabled" href="#">Logout</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    </div>
-
+        </ul>
+    </nav>
   );
 }
 
